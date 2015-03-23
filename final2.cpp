85152d990f8291f9bab67d2aed4b1429cb99120d
rikardo0h <apple_crox@hotmail.com>
2015-03-23 17:05:47 -0600

    h

--- final2.cpp
+++ final2.cpp
@@ -134,7 +134,7 @@ int main(int argc, char **argv) {
           if(read)      
         {
             memcpy(&message, buffer, 16);
-        	printf("%s %i\n", message.org, message.dest);
+        	printf("%c %c \n", message.org, message.dest);
         }
         
         // check for data on port and display it on screen.
